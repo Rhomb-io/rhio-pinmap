2e28d45c4e04bacdc9cfb3688cf286a6d00bd616
german <microkom@gmail.com>
2020-04-26 14:17:12 +0200

    Removed some comments

--- examples/blink/blink.ino
+++ examples/blink/blink.ino
@@ -4,19 +4,6 @@
  * @brief Blink the built in led of any rhomb.io Duino Master
  */
 #include "Arduino.h"
-
-// List of supported MCUs. Uncomment ONLY one.
-// #define DUINO_UNO_v1
-// #define DUINO_LEONARDO_v1
-// #define DUINO_PRO_v1
-// #define DUINO_ZERO_v1
-// #define DUINO_MEGA_v0
-// #define DUINO_MEGA_v1
-// #define ESP32_v1
-// #define ESP32_v2
-// #define ESP32_v1
-// #define ESP8266_v1
-// #define STM32L476_v1
 #include "rhio-pinmap.h"
 
 void setup () {
